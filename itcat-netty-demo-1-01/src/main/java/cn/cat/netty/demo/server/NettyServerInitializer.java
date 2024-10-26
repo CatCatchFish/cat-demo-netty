48b84855f50dc796018e249606fbc88e73890d5d
CatFish <3285109052@qq.com>
2024-10-26 10:49:17 +0800

    NettyServer

--- itcat-netty-demo-1-01/src/main/java/cn/cat/netty/demo/server/NettyServerInitializer.java
+++ itcat-netty-demo-1-01/src/main/java/cn/cat/netty/demo/server/NettyServerInitializer.java
@@ -9,11 +9,13 @@ public class NettyServerInitializer extends ChannelInitializer<SocketChannel> {
     private static final Logger logger = LoggerFactory.getLogger(NettyServerInitializer.class);
 
     @Override
-    protected void initChannel(SocketChannel socketChannel) throws Exception {
+    protected void initChannel(SocketChannel channel) throws Exception {
         logger.info("init channel");
         logger.info("there is a new client connected");
-        logger.info("client ip:{}", socketChannel.localAddress().getHostName());
-        logger.info("client port:{}", socketChannel.localAddress().getPort());
+        logger.info("client ip:{}", channel.localAddress().getHostName());
+        logger.info("client port:{}", channel.localAddress().getPort());
         logger.info("channel initialized");
+
+        channel.pipeline().addLast(new MyServerHandler());
     }
 }
